0dfc9b18aee52f09d13497c41f7ca330fb09f2cf
Vansh Tyagi <vansh.tyagi1260@gmail.com>
2026-06-10 21:48:56 +0530

    LC 392

---
+++ Week_2/Day_3/issubsequence.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int i=0,j=0;
+
+        while(j<t.size()){
+
+            if(i==s.size()){
+                return true;
+            }
+
+            if(s[i]==t[j]){
+                i++;
+                j++;
+            }
+            else{
+                j++;
+            }
+        }
+
+        return i==s.size();
+    }
+};
